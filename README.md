69bc53a71e9c1fbcb8ffcf25e54be325cb277855
Luca <ldm.marra@gmail.com>
2022-04-27 22:10:02 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # obsidian-garden-generator
 Python script to create a digital garden using an Obsidian vault as input
+
+The script is executable is called `obgage`
+
+You need to create a `config.ini` file with the following content:
+
+```
+[DEFAULT]
+BASE_DIR = /obsidian/vault/dir
+START_PAGE = home
+OUTPUT_DIR = output
+```
+
+This config opens `home.md` file in the Obsidian vault and starts creating output files in `output` directory.
+
+## Todo
+- templates should be external from this project
---
+++ obsidian_garden_generator/obgage.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+from collections import defaultdict
+import markdown
+import re
+from os import path
+from jinja2 import Environment, PackageLoader 
+from slugify import slugify
+from configparser import ConfigParser
+
+
+LINK_REGEX = r'\[\[([^\]\[]+\|)?([^\]\[]+)\]\]'
+
+
+def load_config():
+    config = ConfigParser()
+    try:
+        config.read_file(open("config.ini"))
+    except FileNotFoundError:
+        print("Missing config.ini")
+        exit(1)
+    return config["DEFAULT"]
+
+
+env = Environment(
+    loader=PackageLoader("obsidian_garden_generator")
+)
+
+config = load_config()
+
+
+class Page:
+    name: str
+    is_index: bool
+    markdown: str
+    html: str
+    links: set[Page]
+    backlinks: set[Page]
+    
+    def __init__(self, name, is_index=False) -> None:
+        self.name = name
+        self.is_index = is_index
+        self.markdown = ""
+        self.html = ""
+        self.links = set()
+        self.backlinks = set()
+        
+    def __repr__(self) -> str:
+        return self.name
+    
+    def __str__(self) -> str:
+        return self.name
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+    def __eq__(self, __o: object) -> bool:
+        return self.name == __o.name
+    
+    @property
+    def markdown_name(self) -> str:
+        return f"{self.name}.md"
+
+    @property
+    def html_name(self) -> str:
+        filename = "index" if self.is_index else slugify(self.name)
+        return f"{filename}.html"
+    
+    def parse(self):
+        with open(path.join(config["BASE_DIR"], self.markdown_name), 'r') as f:
+            self.markdown = f.read()
+        self.links = {Page(link[0][:-1]) if link[0] else Page(link[1]) for link in re.findall(LINK_REGEX, self.markdown)}
+        html = markdown.markdown(self.markdown, extensions=["codehilite", "fenced_code"])
+        self.html = re.sub(LINK_REGEX, self._create_link, html)
+        self.compute_backlinks()
+    
+    @staticmethod
+    def _create_link(match) -> str:
+        name = match.group(2)
+        if link := match.group(1):
+            filename = slugify(link[:-1]) + '.html'
+        else:
+            filename = slugify(name) + '.html'
+        return f'<a href="{filename}">{name}</a>'
+
+    def compute_backlinks(self):
+        for link in self.links:
+            backlinks[link].add(self)
+
+    def save(self):
+        template = env.get_template("index.html") 
+        content = template.render(content=self.html, backlinks=sorted(backlinks[self], key=lambda x: 0 if x.name == config["START_PAGE"] else 1))
+        
+        with open(path.join(config["OUTPUT_DIR"], self.html_name), 'w') as f:
+            f.write(content)
+
+
+backlinks = defaultdict(set)
+pages = set()
+
+
+def create_page(name, is_index=False):
+    page = Page(name, is_index)
+    page.parse()
+    pages.add(page)
+
+    for link in page.links:
+        create_page(link.name)
+
+
+def run():
+    create_page(config["START_PAGE"], is_index=True)
+    
+    for page in pages:
+        page.save()
